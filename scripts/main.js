6d8dd9b0176eb29489992626d7e3817879e75b34
Hermes J. Pappas <hjp@hjp.gr>
2021-12-11 19:09:12 +0200

    Update to accept city as parameter

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,11 @@
-let requestFront = 'https://api.openweathermap.org/data/2.5/weather?q=';
-let requestBack = '&appid=a0fea6cd1c37ef074fec6088abfdb5fb';
+let requestFront = "https://api.openweathermap.org/data/2.5/weather?q=";
+let requestBack = "&appid=a0fea6cd1c37ef074fec6088abfdb5fb";
 
-let getWeather = async () => {
-let response = await fetch(requestFront + 'Athens,GR' + requestBack);
-let result = await response.json();
+async function getWeather(city) {
+  let response = await fetch(requestFront + city + requestBack);
+  let result = await response.json();
 
-console.log(result);
-
+  console.log(result);
 }
 
-getWeather();
+getWeather("Athens");
