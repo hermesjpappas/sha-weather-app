7f556d859c2707786ab011697bac7aca5ff5a5f6
Hermes J. Pappas <hjp@hjp.gr>
2021-12-11 19:09:47 +0200

    Change variable name

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,9 @@ let requestBack = "&appid=a0fea6cd1c37ef074fec6088abfdb5fb";
 
 async function getWeather(city) {
   let response = await fetch(requestFront + city + requestBack);
-  let result = await response.json();
+  let data = await response.json();
 
-  console.log(result);
+  console.log(data);
 }
 
 getWeather("Athens");
