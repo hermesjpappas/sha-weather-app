8c11899f5ae84444d05893d867c335d1ba597da7
Hermes J. Pappas <hjp@hjp.gr>
2021-12-11 19:11:17 +0200

    Change function to return data to later process

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,13 @@
 let requestFront = "https://api.openweathermap.org/data/2.5/weather?q=";
 let requestBack = "&appid=a0fea6cd1c37ef074fec6088abfdb5fb";
 
-async function getWeather(city) {
+async function getWeatherData(city) {
   let response = await fetch(requestFront + city + requestBack);
   let data = await response.json();
 
   console.log(data);
+
+  return data;
 }
 
-getWeather("Athens");
+getWeatherData("Athens");
