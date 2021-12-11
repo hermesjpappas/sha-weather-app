523eb60e7699db3c69bd2813536e0afe2c862df9
Hermes J. Pappas <hjp@hjp.gr>
2021-12-11 19:29:24 +0200

    First attempt at using data

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,23 @@ let requestFront = "https://api.openweathermap.org/data/2.5/weather?q=";
 let requestBack = "&appid=a0fea6cd1c37ef074fec6088abfdb5fb";
 
 async function getWeatherData(city) {
-  let response = await fetch(requestFront + city + requestBack);
-  let data = await response.json();
 
-  console.log(data);
+  try {
+    let response = await fetch(requestFront + city + requestBack);
+    let data = await response.json();
+    return data;
+  }
 
-  return data;
+  catch (e) {
+    console.log(e);
+  }
 }
 
-getWeatherData("Athens");
+let athensData = getWeatherData("Athens");
+athensData.then(data => displayWeatherData(data));
+
+
+
+function displayWeatherData(data) {
+  console.log(data.name, "Temperature:", data.main.temp,"Weather:",data.weather[0].description);
+}
